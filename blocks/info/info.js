7e7c575a15c69718d6b6a09c1ca146a46ba44c01
Muthu28031993 <MuthuSoundaraPandian.P@cognizant.com>
2026-04-15 15:26:16 +0530

    logo

--- blocks/info/info.js
+++ blocks/info/info.js
@@ -2,4 +2,28 @@ import { decorateIcons } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   decorateIcons(block);
+
+  const infoSection = block.closest('.info-container');
+
+  if (!infoSection) return;
+
+  const moveBeforeHeader = () => {
+    const header = document.querySelector('.header-wrapper');
+    if (header) {
+      header.before(infoSection);
+      return true;
+    }
+    return false;
+  };
+
+  if (moveBeforeHeader()) return;
+  const observer = new MutationObserver(() => {
+    if (moveBeforeHeader()) {
+      observer.disconnect();
+    }
+  });
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 }
